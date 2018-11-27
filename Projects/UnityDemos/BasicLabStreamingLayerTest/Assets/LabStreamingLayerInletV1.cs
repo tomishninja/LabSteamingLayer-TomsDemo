2502cd46e0127e3a51b92825d2b181dfa8bd2eb5
tomishninja <tomishninja@gmail.com>
2018-11-27 15:39:23 +1030

    built some unity demos and started testing limits of the system

---
+++ Projects/UnityDemos/BasicLabStreamingLayerTest/Assets/LabStreamingLayerInletV1.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LSL;
+
+public class LabStreamingLayerInletV1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // wait until an EEG stream shows up
+        liblsl.StreamInfo[] results = liblsl.resolve_stream("type", "Markers");
+
+        // open an inlet and print meta-data
+        liblsl.StreamInlet inlet = new liblsl.StreamInlet(results[0]);
+        System.Console.Write(inlet.info().as_xml());
+
+        // read samples
+        string[] sample = new string[1];
+        while (true)
+        {
+            inlet.pull_sample(sample);
+            Debug.Log(sample[0]);
+        }
+    }
+}
---
+++ Projects/UnityDemos/BasicLabStreamingLayerTest/Assets/newScriptInletForDemo.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using UnityEngine;
+using Assets.LSL4Unity.Scripts.AbstractInlets;
+using Assets.LSL4Unity.Scripts;
+
+public class newScriptInletForDemo : InletFloatSamples
+{
+
+    private bool pullSamplesContinuously = false;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    protected override bool isTheExpected(LSLStreamInfoWrapper stream)
+    {
+        // the base implementation just checks for stream name and type
+        var predicate = base.isTheExpected(stream);
+        // add a more specific description for your stream here specifying hostname etc.
+        //predicate &= stream.HostName.Equals("Expected Hostname");
+        return predicate;
+    }
+    /// <summary>
+    /// Override this method to implement whatever should happen with the samples...
+    /// IMPORTANT: Avoid heavy processing logic within this method, update a state and use
+    /// coroutines for more complexe processing tasks to distribute processing time over
+    /// several frames
+    /// </summary>
+    /// <param name="newSample"></param>
+    /// <param name="timeStamp"></param>
+    protected override void Process(float[] newSample, double timeStamp)
+    {
+        Debug.Log(timeStamp);
+        string text = "";
+        // What should this process do with what its given?
+        for(int index = 0; index < newSample.Length; index++)
+        {
+            text += newSample[index];
+            if (index != newSample.Length - 1)
+            {
+                text += ", ";
+            }
+        }
+
+        Debug.Log(text);
+    }
+    
+    protected override void OnStreamAvailable()
+    {
+        pullSamplesContinuously = true;
+    }
+    
+    protected override void OnStreamLost()
+    {
+        pullSamplesContinuously = false;
+    }
+
+    private void Update()
+    {
+        if (pullSamplesContinuously)
+        {
+            pullSamples();
+            Debug.Log("SamplesPulled");
+        }
+    }
+}
